8b6348a8a01d4f800de62aa8a5b55aa0176699a9
wytan-dev <tanweeyi650@hotmail.com>
2025-03-11 22:37:17 +0800

    Minor fix

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require("discord.js");
 
 module_exports = {
-    data = new SlashCommandBuilder()
+    data: new SlashCommandBuilder()
         .setName("ping")
         .setDescription("Replies with pong!"),
     async execute(interaction) {
